feee604fee12c159747d2c676bd7498b7bbf556a
lpszeto <lpszeto@gmail.com>
2020-01-27 20:38:37 -0800

    Checkpoints 6

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -120,7 +120,9 @@ public class CakeView extends SurfaceView {
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
         //Now a candle in the center
-        drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
+        drawCandle(canvas, cakeLeft + cakeWidth/4 - candleWidth/2, cakeTop);
+        //Second Candle
+        drawCandle(canvas, cakeLeft + cakeWidth/1.5f - candleWidth/2, cakeTop);
 
     }//onDraw
 
